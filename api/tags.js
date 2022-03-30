c1dd206701301013a04781db7951a60a1edefa80
Wyatt Wilson <wyattwilson916@gmail.com>
2022-03-30 15:15:39 -0400

    Wrapping up

--- api/tags.js
+++ api/tags.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const tagsRouter = express.Router();
-const { getAllTags } = require("../db");
+const { getAllTags, getPostsByTagName } = require("../db");
 
 tagsRouter.use((req, res, next) => {
   console.log("A request is being made to /tags");
@@ -8,12 +8,32 @@ tagsRouter.use((req, res, next) => {
   next();
 });
 
+tagsRouter.get("/", async (req, res, next) => {
+  try {
+    const tags = await getAllTags();
+    res.send({
+      tags: [],
+    });
+    
+  } catch ({ name, message }) {
+    next({ name, message })
+  }
+});
 
-tagsRouter.get("/", async (req, res) => {
-  const tags = await getAllTags();
-  res.send({
-    tags: []
-  });
+tagsRouter.get("/:tagName/posts", async (req, res, next) => {
+  const {tagName} = req.params;
+
+  try {
+    const allPosts = await getPostsByTagName(tagName);
+    const posts = allPosts.filter((post) => {
+      return post.active || (req.user && post.author.id === req.user.id);
+    });
+    res.send({
+      posts,
+    });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 });
 
 module.exports = tagsRouter;
